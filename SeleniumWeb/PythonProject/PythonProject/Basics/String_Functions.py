20c6d7e249c5e1592811e0cd055d764f57c76f7f
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-07-10 14:27:07 +0600

    string functions

---
+++ SeleniumWeb/PythonProject/PythonProject/Basics/String_Functions.py
@@ -0,0 +1,20 @@
+# capitalize(): Converts the first character of a string to uppercase.
+# casefold(): Converts the entire string to lowercase, similar to lower(), but more aggressive in handling special characters.
+# center(width, fillchar): Returns a centered string by padding it with fillchar (default is space) on both sides to reach the specified width.
+# count(substring, start, end): Returns the number of occurrences of substring within the string, optionally within the specified range.
+# encode(encoding, errors): Returns an encoded version of the string using the specified encoding (e.g., ‘utf-8’).
+# endswith(suffix, start, end): Checks if the string ends with the specified suffix.
+# join(iterable): Joins the elements of an iterable (e.g., a list) into a single string using the original string as a separator.
+# replace(old, new, count): Replaces occurrences of old with new in the string. You can limit the number of replacements with count.
+# split(separator, maxsplit): Splits the string into a list of substrings based on the specified separator. You can limit the number of splits with maxsplit.
+# strip(chars): Removes leading and trailing characters specified in chars (default is whitespace).
+# startswith(prefix, start, end): Checks if the string starts with the specified prefix.
+# upper(): Converts the entire string to uppercase.
+# lower(): Converts the entire string to lowercase.
+# isalpha(): Returns True if all characters in the string are alphabetic.
+# isdigit(): Returns True if all characters in the string are digits.
+# isnumeric(): Returns True if all characters in the string are numeric.
+# isalnum(): Returns True if all characters in the string are alphanumeric.
+# isspace(): Returns True if all characters in the string are whitespace.
+# startswith(prefix): Returns True if the string starts with the specified prefix.
+# find(substring, start, end): Searches for substring in the string and returns its position (or -1 if not found).
