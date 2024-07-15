b04de5005c5bebd64cbc97b5e63503652cab1e96
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-07-15 16:03:43 +0600

    rghab

---
+++ SeleniumWebRaghab/pythonProject1/Tutorial/chrome_driver.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+chrome_driver.maximize_window()
+chrome_driver.get("https://google.com")
+chrome_driver.implicitly_wait(time_to_wait= 2)
+
+print(chrome_driver.title)
+chrome_driver.close()
+chrome_driver.quit()
---
+++ SeleniumWebRaghab/pythonProject1/Tutorial/elements.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import time
+
+edge_driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+edge_driver.maximize_window()
+edge_driver.get("https://jobs.hi-bd.org/")
+
+time.sleep(5)
+
+print(edge_driver.title)
+
+login_button = edge_driver.find_element(By.XPATH, '//*[@id="mobile-nav-menu"]/ul/li[2]/a')
+login_button.click()
+
+time.sleep(5)
+
+print(edge_driver.title)
