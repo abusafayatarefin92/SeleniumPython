462e5bc0d2d86411c81e9a69b85a14b698356d03
abusafayatarefin92@gmail.com <abusafayatarefin92@gmail.com>
2024-07-18 11:03:22 +0600

    july 18

--- SeleniumWebRaghab/pythonProject/Tutorial/elements.py
+++ SeleniumWebRaghab/pythonProject/Tutorial/elements.py
@@ -19,5 +19,16 @@ login_button.click()
 time.sleep(5)
 
 print(edge_driver.title)
+
+your_email = edge_driver.find_element(By.XPATH, '//*[@id="form2Example11"]')
+your_email.send_keys('safayatarefin@gmail.com')
+password = edge_driver.find_element(By.XPATH, '//*[@id="form2Example22"]')
+password.send_keys('123456')
+log_in = edge_driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/section/div/div/div/div/div[2]/div/div/div/form/div[3]/button')
+log_in.click()
+
+print(edge_driver.title)
+
+time.sleep(5)
 edge_driver.close()
 edge_driver.quit()
