672a3f2ea0e9c0d9674747c0f3b9bc3dfdda0e21
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-07-28 11:07:04 +0600

    office

--- SeleniumWebRaghab/pythonProject/HANDICAP/Application_Panel/applicant_signup.py
+++ SeleniumWebRaghab/pythonProject/HANDICAP/Application_Panel/applicant_signup.py
@@ -7,6 +7,6 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get("https://jobs.hi-bd.org/login")
 
-driver.find_element(By.NAME, 'email').send_keys("mahbub.stc0510@gmail.com")
-driver.find_element(By.NAME, 'password').send_keys("Mahbub@0510")
+driver.find_element(By.NAME, 'email').send_keys("safayatarefin@gmail.com")
+driver.find_element(By.NAME, 'password').send_keys("123456")
 driver.find_element(By.XPATH, '//button[contains(text(),"Log")]').click()
