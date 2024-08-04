cbb1c274526a5e344f3604b43a26e5bf5707dfbe
abusafayatarefin92@gmail.com <abusafayatarefin92@gmail.com>
2024-08-04 11:21:18 +0600

    pytest

--- SeleniumWebRaghab/pythonProject/HANDICAP/Application_Panel/applicant_signup.py
+++ SeleniumWebRaghab/pythonProject/HANDICAP/Application_Panel/applicant_signup.py
@@ -2,10 +2,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-
+# driver = webdriver.Chrome()
+driver = webdriver.Edge()
 driver.implicitly_wait(5)
 driver.get("https://jobs.hi-bd.org/login")
+driver.maximize_window()
 
 driver.find_element(By.NAME, 'email').send_keys("safayatarefin@gmail.com")
 driver.find_element(By.NAME, 'password').send_keys("123456")
---
+++ SeleniumWebRaghab/pythonProject/Pytest/test_demo1.py
@@ -0,0 +1,11 @@
+def func(x):
+    return x + 1
+
+def test_1():
+    assert func(3) == 4
+
+def test_2():
+    assert func(4) == 5
+
+def test_3():
+    assert func(5) == 6
