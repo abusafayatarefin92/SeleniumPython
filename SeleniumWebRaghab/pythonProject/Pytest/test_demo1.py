da8dbe9bda434c0f7fd03a37daa2914dc20e9bdd
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-08-18 14:42:16 +0600

    2024 08 18

--- SeleniumWebRaghab/pythonProject/Pytest/test_demo1.py
+++ SeleniumWebRaghab/pythonProject/Pytest/test_demo1.py
@@ -1,11 +1,16 @@
 def func(x):
     return x + 1
 
-def test_1():
-    assert func(3) == 4
+class TestCases:
+    def test_1(self):
+        assert func(3) == 4
 
-def test_2():
-    assert func(4) == 5
+    def test_2(self):
+        assert func(4) == 5
 
-def test_3():
-    assert func(5) == 6
+    def test_3(self):
+        assert func(5) == 6
+
+# pytest .\Pytest\test_demo1.py
+# pytest .\Pytest\test_demo1.py -k 'test_1'
+# python -m pytest .\Pytest\test_demo1.py
---
+++ SeleniumWebRaghab/pythonProject/selenium_pytest_demo1.py
@@ -0,0 +1,37 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+import time
+
+@pytest.fixture()
+def edge_driver_handicap():
+    edge_driver_handicap = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    edge_driver_handicap.maximize_window()
+    edge_driver_handicap.implicitly_wait(10)
+
+    yield edge_driver_handicap
+
+    edge_driver_handicap.close()
+    edge_driver_handicap.quit()
+
+def test_handicap_login(edge_driver_handicap):
+    edge_driver_handicap.get("https://jobs.hi-bd.org/")
+
+    time.sleep(5)
+
+    login_button = edge_driver_handicap.find_element(By.XPATH, '//*[@id="mobile-nav-menu"]/ul/li[2]/a')
+    login_button.click()
+
+    time.sleep(5)
+
+    your_email = edge_driver_handicap.find_element(By.XPATH, '//*[@id="form2Example11"]')
+    your_email.send_keys('safayatarefin@gmail.com')
+    password = edge_driver_handicap.find_element(By.XPATH, '//*[@id="form2Example22"]')
+    password.send_keys('123456')
+    log_in = edge_driver_handicap.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/section/div/div/div/div/div[2]/div/div/div/form/div[3]/button')
+    log_in.click()
+
+    time.sleep(5)
+    print('Test Completed')
