3586bbf59c6075dea89b6b80b22b18c3d7d61ac3
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2024-09-01 16:03:26 +0600

    2024 09 01

--- SeleniumWeb/PythonProject/PythonProject/Basics/multilevel_inharitance.py
+++ SeleniumWeb/PythonProject/PythonProject/Basics/multilevel_inharitance.py
@@ -5,31 +5,27 @@ class MoveCharacter:
     def move_bwd(self):
         print('Move backward 1 step')
 
-class JumpCharacter:
+class JumpCharacter(MoveCharacter):
     def jump_1_level(self):
         print('Jump character 1 level')
 
     def jump_2_level(self):
         print('Jump character 2 level')
 
-class Pokeman(MoveCharacter, JumpCharacter):
+class Pokeman(JumpCharacter):
     def mov_fwd(self):
         print('Pokeman moving')
 
-pokeman = Pokeman()
-pokeman.mov_fwd()
-pokeman.move_bwd()
-pokeman.jump_1_level()
-pokeman.jump_2_level()
-
-class Micky(MoveCharacter, JumpCharacter):
+class Micky(Pokeman):
     def jump_1_level(self):
         print('Micky is jumping')
+
+    def mov_fwd(self):
+        print('Micky moving')
 
 micky = Micky()
 micky.mov_fwd()
 micky.move_bwd()
 micky.jump_1_level()
 micky.jump_2_level()
-print(Pokeman.mro())
 print(Micky.mro())
